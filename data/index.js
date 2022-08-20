30eb72df403e3a69ceb0bf0787d3ba9f319026ce
pblgllgs <pbl.gllgs@gmail.com>
2022-08-19 20:48:28 -0400

    local data fetch

---
+++ data/index.js
@@ -0,0 +1,4 @@
+export * as mostPopular from "./mostPopular.json";
+export * as programming from "./programming.json";
+export * as series from "./series.json";
+export * as space from "./space.json";
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { Banner, Navbar, SectionCard } from "../components";
-import { getPopularVideos, getVideos } from "../lib/videos";
+import {
+  getCommonVideosLocal,
+  getPopularVideos,
+  getVideos,
+} from "../lib/videos";
+import { mostPopular, series, programming, space } from "../data";
+import { env } from "../config/config";
 
 export default function Home({
   actionVideos,
@@ -38,10 +44,24 @@ export default function Home({
 }
 
 export const getServerSideProps = async () => {
-  const actionVideos = await getVideos("razer");
-  const programmingVideos = await getVideos("programming");
-  const spaceVideos = await getVideos("space");
-  const popularVideos = await getPopularVideos();
+  let actionVideos = [];
+  let programmingVideos = [];
+  let spaceVideos = [];
+  let popularVideos = [];
+  const environment = env();
+  switch (environment) {
+    case "local":
+      actionVideos = await getCommonVideosLocal(series);
+      programmingVideos = await getCommonVideosLocal(programming);
+      spaceVideos = await getCommonVideosLocal(space);
+      popularVideos = await getCommonVideosLocal(mostPopular);
+      break;
+    case "web":
+      actionVideos = await getVideos("series");
+      programmingVideos = await getVideos("programming");
+      spaceVideos = await getVideos("space");
+      popularVideos = await getPopularVideos();
+  }
   return {
     props: {
       actionVideos,
