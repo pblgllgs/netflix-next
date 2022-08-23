55708f960a3076e467b53f410d1e099585fe6e37
pblgllgs <pbl.gllgs@gmail.com>
2022-08-22 21:24:34 -0400

    updates, index, home

--- components/banner/Banner.js
+++ components/banner/Banner.js
@@ -5,10 +5,7 @@ import styles from "./Banner.module.css";
 
 const Banner = (props) => {
   const router = useRouter();
-  const { title, subTitle, imgUrl } = props;
-  const handleOnPlay = () => {
-    console.log("play");
-  };
+  const { imgUrl } = props;
 
   const handleToLogin = () => {
     router.push("/login");
@@ -16,46 +13,58 @@ const Banner = (props) => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.leftWrapper}>
-        <div className={styles.imgLogo}>
-          <Link href="/home" passHref>
-            <a className={styles.logoLink}>
-              <div className={styles.logoWrapper}>
-                <Image
-                  src="/static/netflix.svg"
-                  alt="play"
-                  width={"150px"}
-                  height={"50px"}
-                />
-              </div>
-            </a>
-          </Link>
-        </div>
-        <div className={styles.right}>
-          <button onClick={handleToLogin} className={styles.btnIniciar}>
-            iniciar sesión
-          </button>
-        </div>
-        <div className={styles.left}>
-          <div className={styles.nseriesWrapper}>
-            <p className={styles.firstLetter}>N</p>
-            <p className={styles.series}>S E R I E S</p>
+      <div className={styles.mainWrapper}>
+        <div className={styles.leftWrapper}>
+          <div className={styles.uno}>
+            <div className={styles.imgLogo}>
+              <Link href="/home" passHref>
+                <a className={styles.logoLink}>
+                  <div className={styles.logoWrapper}>
+                    <Image
+                      src="/static/netflix.svg"
+                      alt="play"
+                      width={"150px"}
+                      height={"50px"}
+                    />
+                  </div>
+                </a>
+              </Link>
+            </div>
+            <div className={styles.right1}>
+              <button onClick={handleToLogin} className={styles.btnIniciar}>
+                iniciar sesión
+              </button>
+            </div>
           </div>
-          <h3 className={styles.title}>{title}</h3>
-          <h3 className={styles.subTitle}>{subTitle}</h3>
-          <div className={styles.playBtnWrapper}>
-            <button className={styles.btnWithIcon} onClick={handleOnPlay}>
-              <Image
-                src="/static/play.svg"
-                alt="play"
-                width={"32px"}
-                height={"32px"}
-              />
-              <span className={styles.playText}>Play</span>
-            </button>
+          <div className={styles.dos}>
+            <div className={styles.cental}>
+              <div className={styles.subTitle1}>Películas y series</div>
+              <div className={styles.subTitle12}>ilimitadas y mucho más</div>
+              <div className={styles.subTitle2}>
+                Disfruta donde quieras. Cancela cuando quieras.
+              </div>
+              <div className={styles.subTitle3}>
+                ¿Quieres ver Netflix ya? Ingresa tu email para crear una cuenta
+                o reiniciar tu membresía
+              </div>
+              <div className={styles.subTitle3}>de Netflix.</div>
+              <div className={styles.formIndex}>
+                <div className={styles.inputIndex}>
+                  <input
+                    placeholder="Email"
+                    className={styles.inputIndex}
+                    type="text"
+                  />
+                </div>
+                <div>
+                  <button className={styles.btnInputIndex}>Comenzar</button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+
       <div
         className={styles.bannerImg}
         style={{
--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,82 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { Banner } from "../components";
-import {
-  getCommonVideosLocal,
-  getPopularVideos,
-  getVideos,
-} from "../lib/videos";
-import { mostPopular, series, programming, space } from "../data";
-import { env } from "../config/config";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
+  const router = useRouter();
+  const handleToLogin = () => {
+    router.push("/login");
+  };
   return (
     <div className={styles.container}>
       <Head>
         <title>Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.main}>
-        <Banner
-          title={"Mr robot"}
-          subTitle={"Have society?"}
-          imgUrl="/static/mrrobot.jpg"
-        />
+      <div className={styles.mainWrapper}>
+        <div className={styles.leftWrapper}>
+          <div className={styles.uno}>
+            <div className={styles.imgLogo}>
+              <Link href="/" passHref>
+                <a className={styles.logoLink}>
+                  <div className={styles.logoWrapper}>
+                    <Image
+                      src="/static/netflix.svg"
+                      alt="play"
+                      width={"150px"}
+                      height={"50px"}
+                    />
+                  </div>
+                </a>
+              </Link>
+            </div>
+            <div className={styles.blockRight}>
+              <div className={styles.btnLenguaje}>
+                <button>Español</button>
+                <Image
+                  src="/static/arrow_drop_down.svg"
+                  alt="fecha"
+                  width={"32px"}
+                  height={"32px"}
+                />
+              </div>
+              <div className={styles.btnInit}>
+                <button onClick={handleToLogin} className={styles.btnIniciar}>
+                  iniciar sesión
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className={styles.dos}>
+            <div className={styles.cental}>
+              <div className={styles.subTitle1}>Películas y series</div>
+              <div className={styles.subTitle12}>ilimitadas y mucho más</div>
+              <div className={styles.subTitle2}>
+                Disfruta donde quieras. Cancela cuando quieras.
+              </div>
+              <div className={styles.subTitle3}>
+                ¿Quieres ver Netflix ya? Ingresa tu email para crear una cuenta
+                o reiniciar tu membresía
+              </div>
+              <div className={styles.subTitle4}>de Netflix.</div>
+              <div className={styles.formIndex}>
+                <div className={styles.inputIndex}>
+                  <input
+                    placeholder="Email"
+                    className={styles.inputIndex}
+                    type="text"
+                  />
+                </div>
+                <div>
+                  <button className={styles.btnInputIndex}>Comenzar</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
-
-export const getServerSideProps = async () => {
-  let actionVideos = [];
-  let programmingVideos = [];
-  let spaceVideos = [];
-  let popularVideos = [];
-  const environment = env();
-  switch (environment) {
-    case "local":
-      actionVideos = await getCommonVideosLocal(series);
-      programmingVideos = await getCommonVideosLocal(programming);
-      spaceVideos = await getCommonVideosLocal(space);
-      popularVideos = await getCommonVideosLocal(mostPopular);
-      break;
-    case "web":
-      actionVideos = await getVideos("series");
-      programmingVideos = await getVideos("programming");
-      spaceVideos = await getVideos("space");
-      popularVideos = await getPopularVideos();
-  }
-  return {
-    props: {
-      actionVideos,
-      programmingVideos,
-      spaceVideos,
-      popularVideos,
-    },
-  };
-};
